4d6a1f519131385e3d8874b0e5c7e5455de8f6bc
nagaraj6618 <nagaraj516700@gmail.com>
2023-06-10 23:49:01 +0530

    Condition Added for posting email

--- src/components/SignUpComponent/SignUpComponent.jsx
+++ src/components/SignUpComponent/SignUpComponent.jsx
@@ -98,7 +98,7 @@ class SignUpComponent extends Component {
         <div className='mb-3'>
           <label>Email</label>
           <input
-          type='email'
+          type='text'
           className='form-control'
           placeholder='Enter your email'
           required
